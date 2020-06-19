2f4a1d289a58b459cf25e53a15c90f36d7191204
hiweibolu <53548456+hiweibolu@users.noreply.github.com>
2020-06-19 22:45:38 +0800

    Add files via upload

--- myhash.hpp
+++ myhash.hpp
@@ -1,10 +1,10 @@
 word myHash(const char *str) {
-	unsigned short h1 = 89, h2 = 997;
+	unsigned short h1 = 7, h2 = 41;
 
 	int len = strlen(str);
 	for (int i = 0; i < len; i++) {
-		h1 = h1 * 83 + str[i];
-		h2 = h2 * 263 + str[i];
+		h1 = h1 * 97 + str[i];
+		h2 = h2 * 43 + str[i];
 	}
 	return (((word)h1)<<16)+(word)h2;
 }
